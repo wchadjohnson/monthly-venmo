299dde33bc7641242d7f0d407168b5928b1e096e
Chad Johnson <wchadjohnson@gmail.com>
2024-04-13 23:21:05 -0500

    moved pay note to a variable

--- pay.py
+++ pay.py
@@ -16,13 +16,14 @@ def main(now):
   VENMO_PAY_USERNAME=get_env('VENMO_PAY_USERNAME')
   venmo_fund_source=get_env('VENMO_FUND_SOURCE')
   pay_amount=float(get_env('PAY_AMOUNT'))
+  pay_note=get_env('PAY_NOTE')
  
   #month = get_month(now)
   venmo = Client(access_token=access_token)
   #telegram = Telegram(bot_token, chat_id)
 
   user = venmo.user.get_user_by_username(VENMO_PAY_USERNAME)
-  venmo.payment.send_money(amount=pay_amount, note='Lawn Care Services - 4 cuts', target_user=user, funding_source_id=venmo_fund_source)
+  venmo.payment.send_money(amount=pay_amount, note=pay_note, target_user=user, funding_source_id=venmo_fund_source)
 
 #  if success:
 #    print("✅ Ran Vemo Pay script successfully")
