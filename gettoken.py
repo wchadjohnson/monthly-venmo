23ab99c1c806290d4bd9de20e181eec883972f62
Chad Johnson <wchadjohnson@gmail.com>
2024-04-14 21:39:36 -0500

    Update gettoken.py
    Fixed security

--- gettoken.py
+++ gettoken.py
@@ -3,7 +3,7 @@ from venmo_api import Client
 # Get your access token. You will need to complete the 2FA process
 # Please store it somewhere safe and use it next time
 # Never commit your credentials or token to a git repository
-access_token = Client.get_access_token(username='wchadjohnson@gmail.com',
-                                        password='meqzyk-xiprom-rugbU7')
+access_token = Client.get_access_token(username='',
+                                        password='')
 print("My token:", access_token)
 
