041c6aa012e3ecc4611c4d91ef927b13cff7e1dc
Chad Johnson <wchadjohnson@gmail.com>
2024-04-20 12:30:38 -0500

    fixing security vulnerability in token retrieval script

--- gettoken.py
+++ gettoken.py
@@ -1,9 +1,13 @@
 from venmo_api import Client
+from dotenv import load_dotenv
+from utils import get_env, env_vars
+
+load_dotenv()
 
 # Get your access token. You will need to complete the 2FA process
 # Please store it somewhere safe and use it next time
 # Never commit your credentials or token to a git repository
-access_token = Client.get_access_token(username='',
-                                        password='')
+access_token = Client.get_access_token(username=get_env('TOKEN_USER'),
+                                        password=get_env('TOKEN_PWD'))
 print("My token:", access_token)
 
